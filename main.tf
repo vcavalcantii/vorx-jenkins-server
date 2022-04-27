0b50e438dd850029c39f2f24eff25b62edc35b71
Vini Cavalcanti <tux.cavalcanti@gmail.com>
2022-04-27 19:43:35 -0300

    Add: EIP for Jenkins Server

--- main.tf
+++ main.tf
@@ -45,3 +45,12 @@ module "ec2_instance" {
     OwnerSRE = "Valfenda"
   }
 }
+
+resource "aws_eip" "jenkins-ip" {
+  instance = module.ec2_instance.id
+  vpc      = true
+
+  tags = {
+    Name = "Jenkins-Server-EIP"
+  }
+}
